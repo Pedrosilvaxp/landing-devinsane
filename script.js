f42420bcfd73080ce21fc5124b74f1e248abbc4d
Pedro Silva do Nascimento <phpedrojs@gmail.com>
2024-07-31 20:30:00 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,96 @@
+// Controlando navbar ao scrollar
+const header = document.querySelector('header');
+let ultimoScr = 0;
+
+window.addEventListener('scroll', function() {
+    let scrollUser = document.documentElement.scrollTop;
+
+    if (scrollUser > ultimoScr) {
+        header.style.top = '-140px';
+    } else {
+        header.style.top = '0px';
+    };
+
+    ultimoScr = scrollUser <= 0 ? 0 : scrollUser;
+});
+
+// Scroll automático
+function cursScroll() {
+    const cursoArea = document.querySelector('.cursos');
+    event.preventDefault();
+    cursoArea.scrollIntoView({behavior:'smooth'});
+};
+function vagaScroll() {
+    const vagaArea = document.querySelector('.vagas');
+    event.preventDefault();
+    vagaArea.scrollIntoView({behavior:'smooth'});
+};
+function sobreScroll() {
+    const sobreArea = document.querySelector('.sobre');
+    event.preventDefault();
+    sobreArea.scrollIntoView({behavior:'smooth'});
+};
+function contScroll() {
+    const contatoArea = document.querySelector('.contato');
+    event.preventDefault();
+    contatoArea.scrollIntoView({behavior:'smooth'});
+};
+
+// Exibindo botão de voltar 
+const backButton = document.querySelector('#back-btn');
+window.onscroll = function() {
+    if (document.documentElement.scrollTop > 600 || document.body.scrollTop > 600) {
+        backButton.style.display = 'block';
+    } else {
+        backButton.style.display = 'none';
+    };
+};
+function voltarScroll() {
+    event.preventDefault();
+    scrollTo({top: 0, behavior:'smooth'});
+};
+
+
+// Menu mobile
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = 'active';
+        this.handleClick = this.handleClick.bind(this);
+        this.handleNavLinkClick = this.handleNavLinkClick.bind(this);
+    };
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+    };
+
+    handleNavLinkClick() {
+        // Remove a classe active quando um link é clicado
+        this.navList.classList.remove(this.activeClass);
+    };
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener('click', this.handleClick);
+        // Adiciona o evento de clique para cada link
+        this.navLinks.forEach(link => {
+            link.addEventListener('click', this.handleNavLinkClick);
+        });
+    };
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        };
+        return this;
+    };
+};
+
+const mobileNavbar = new MobileNavbar(
+    '.mobile-btn',
+    '.container-mob',
+    '.link-mob'
+);
+
+mobileNavbar.init();
